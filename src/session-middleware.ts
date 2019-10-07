0eedd3df10e8027c0960e06c5d0dfb68b668263a
jacobstern <jacob.stern@outlook.com>
2019-10-06 17:17:43 -0700

    Some basic fixes for production build

--- src/session-middleware.ts
+++ src/session-middleware.ts
@@ -1,9 +1,15 @@
 import session from 'express-session';
+import memorystore from 'memorystore';
+
+const MemoryStore = memorystore(session);
 
 export const sessionMiddleware = session({
   secret: 'keyboard cat',
   resave: false,
   saveUninitialized: true,
+  store: new MemoryStore({
+    checkPeriod: 86400000, // prune expired entries every 24h
+  }),
 });
 
 export default sessionMiddleware;
